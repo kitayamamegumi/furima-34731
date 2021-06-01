99490e969ba21823cccb84b5ad95483775cf3661
kitayamamegumi <kitayama9270@yahoo.co.jp>
2021-06-01 20:31:39 +0900

    ユーザー管理機能の実装
    deviseの導入
    新規登録／ログイン／ログアウトが出来る
    モデルの単体テスト実施

---
+++ app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :items
+  has_many :orders
+
+  validates :nickname, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :family_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ一ー]+\z/, message: '全角カナ文字を使用してください' } do
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+end
