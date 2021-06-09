5bb3608c587c607470fe2badd717ae82a2e727fa
kitayamamegumi <kitayama9270@yahoo.co.jp>
2021-06-09 14:27:10 +0900

    商品出品機能の実装　追加修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,13 +7,21 @@ class Item < ApplicationRecord
   validates :discribe,         presence: true
 
   with_options presence: true do
-    validates :category_id, numericality: { other_than: 1 }
-    validates :condition_id, numericality: { other_than: 1 }
-    validates :delivery_pay_id, numericality: { other_than: 1 }
-    validates :prefecture_id, numericality: { other_than: 1 }
-    validates :delivery_day_id, numericality: { other_than: 1 }
+    validates :category_id
+    validates :condition_id
+    validates :delivery_pay_id
+    validates :prefecture_id
+    validates :delivery_day_id
     validates :selling_price, format: { with: /\A-?[0-9]+(\.[0-9]+)?\z/, message: '半角数字を使用してください' },
                               inclusion: { in: (300..9_999_999) }
+  end
+
+  with_options numericality: { other_than: 1, message: 'Select' } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_pay_id
+    validates :prefecture_id
+    validates :delivery_day_id
   end
 
   belongs_to :user
