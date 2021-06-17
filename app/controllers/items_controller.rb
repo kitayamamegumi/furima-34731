0a497439949469e25fd0dbe657d32db8881cd443
kitayamamegumi <kitayama9270@yahoo.co.jp>
2021-06-17 15:10:03 +0900

    商品削除機能の実装　追加修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,12 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id = current_user.id
       @item.destroy
       redirect_to root_path
-    else
-      redirect_to root_path
-    end
   end
 
   private
